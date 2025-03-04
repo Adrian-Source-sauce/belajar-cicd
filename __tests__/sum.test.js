e5003cbd35d074b0848fe791cb781c35a1b86f82
named <named@kali.kali>
2025-03-04 08:34:28 -0500

    move sum.js

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('/sum'); // Sesuaikan dengan path fungsi yang diuji
+const sum = require('../sum'); // Sesuaikan dengan path fungsi yang diuji
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
